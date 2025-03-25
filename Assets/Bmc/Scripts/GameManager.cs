fb71013c98ce1ad91148894dcaab304a7f74510f
bokob <bmc0520@naver.com>
2025-03-25 21:13:34 +0900

    [FEAT] GameManager 기초

---
+++ Assets/Bmc/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance => _instance;
+    static GameManager _instance;
+
+    bool _isPlayerTurn = false;
+    Player _player;
+    Enemy _enemy;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+
+        Init();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Init()
+    {
+        Debug.Log("게임 매니저 생성");
+    }
+
+    public void Test()
+    {
+        Debug.Log("테스트");
+    }
+}
