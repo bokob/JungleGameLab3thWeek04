edc0172d4b397bf662ade623edd17b7cf5793878
bokob <bmc0520@naver.com>
2025-03-25 21:14:35 +0900

    [FEAT] 플레이어 및 적 기초 스크립트

---
+++ Assets/Bmc/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+using Bmc;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Define.PlayState CurrentState => _currentState;
+    // Revolver _revolver;
+    bool _isLive;
+    Define.PlayState _currentState;
+
+    public void ChooseDecision()
+    {
+    }
+
+    public void Play()
+    {
+    }
+}
