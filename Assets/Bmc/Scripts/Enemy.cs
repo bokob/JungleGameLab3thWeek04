a9cc8fd8ba15349b08b7a5b8944ffd9b6e436c87
bokob <bmc0520@naver.com>
2025-03-25 23:48:23 +0900

    [ADD] 주석 추가

--- Assets/Bmc/Scripts/Enemy.cs
+++ Assets/Bmc/Scripts/Enemy.cs
@@ -8,10 +8,12 @@ public class Enemy : MonoBehaviour
     bool _isLive;
     Define.PlayState _currentState;
 
+    // Check일 때 뭐 할지 결정
     public void ChooseDecision()
     {
     }
 
+    // 카드 뽑기
     public void Play()
     {
     }
